75b9ec48f55523e8b0c0b17091407c690cce145e
Churk_Ben <2779675416@qq.com>
2026-07-19 07:18:15 +0800

    feat: 更新图标

--- lib/core/licenses/app_licenses.dart
+++ lib/core/licenses/app_licenses.dart
@@ -8,4 +8,12 @@ void registerAppLicenses() {
     );
     yield LicenseEntryWithLineBreaks(const ['Noto Sans SC'], license);
   });
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString(
+      'assets/icon/LICENSE-FONT-AWESOME.txt',
+    );
+    yield LicenseEntryWithLineBreaks(const [
+      'Font Awesome Free — Calendar Check icon',
+    ], license);
+  });
 }
